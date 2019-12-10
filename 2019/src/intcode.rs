4cf37d1701e758aeb6928c7fd34bfa284052b619
Nick Saika <nicksaika@gmail.com>
2019-12-10 00:29:54 -0400

    intcode: convert to using i64 values, instead of String

--- 2019/src/intcode.rs
+++ 2019/src/intcode.rs
@@ -8,6 +8,7 @@ use std::io::{Error, ErrorKind, Result};
 /// * `&str`
 /// * `String`
 /// * `Vec<String>`
+/// * `Vec<i64>`
 ///
 /// The implementations for `&str` and `String` expect the same input.
 /// The input for these intcode puzzles should be a single line, of
@@ -17,15 +18,21 @@ use std::io::{Error, ErrorKind, Result};
 /// each "," character.
 pub struct Program {
     // Initial state of the program.
-    mem: Vec<String>,
+    mem: Vec<i64>,
 
     // Any input to the program.
-    input: Option<i64>,
+    input: Option<Vec<i64>>,
 }
 
 impl From<&str> for Program {
     fn from(s: &str) -> Self {
-        let mem: Vec<String> = s.split(",").map(|v| v.to_string()).collect();
+        let mem: Vec<i64> = s
+            .split(",")
+            .map(|v| {
+                let n: i64 = v.parse().unwrap();
+                n
+            })
+            .collect();
         Self { mem, input: None }
     }
 }
@@ -38,7 +45,25 @@ impl From<String> for Program {
 
 impl From<Vec<String>> for Program {
     fn from(v: Vec<String>) -> Self {
-        Self { mem: v, input: None }
+        Self {
+            mem: v
+                .iter()
+                .map(|s| {
+                    let n: i64 = s.parse().unwrap();
+                    n
+                })
+                .collect(),
+            input: None,
+        }
+    }
+}
+
+impl From<Vec<i64>> for Program {
+    fn from(v: Vec<i64>) -> Self {
+        Self {
+            mem: v,
+            input: None,
+        }
     }
 }
 
@@ -46,25 +71,41 @@ impl Program {
     /// Set the input for the program to `v`.
     /// This is typically called before `execute`.
     pub fn input(&mut self, v: i64) -> &mut Self {
-        self.input = Some(v);
+        if let Some(input) = &mut self.input {
+            input.push(v);
+            return self;
+        }
+
+        let input: Vec<i64> = vec![v];
+        self.input = Some(input);
         self
+    }
+
+    /// Consumes the first input value provided via the `input` method.
+    /// Successive calls to `get_input` will drain any input provided to the
+    /// program before `execute` or `execute_mut` were called.
+    fn get_input(&mut self) -> Option<i64> {
+        if let Some(input) = &mut self.input {
+            return Some(input.remove(0));
+        }
+        None
     }
 
     /// Execute runs the program the computer was loaded with, and returns
     /// the final memory representation of the program, along with anything
     /// that was output.
-    pub fn execute(&self) -> Result<(Vec<String>, Vec<String>)> {
+    pub fn execute(&mut self) -> Result<(Vec<i64>, Vec<i64>)> {
         let mut mem = self.mem.clone();
-        let mut output: Vec<String> = vec![];
+        let mut output: Vec<i64> = vec![];
         let mut ip: usize = 0; // instruction pointer
         loop {
             // NOTE(nesv): mem[ip] points at the current instruction.
-            let nparams = Instruction::num_params(&mem[ip]);
+            let nparams = Instruction::num_params(mem[ip]);
             if ip + nparams >= mem.len() {
                 break;
             }
 
-            let params: Vec<String> = mem[ip..ip+nparams+1].to_vec();
+            let params: Vec<i64> = mem[ip..ip + nparams + 1].to_vec();
             let inst = Instruction::parse(params);
             //eprintln!("{:?}", &mem);
             //eprintln!("{: <8}{:?}", &ip, &inst);
@@ -72,24 +113,27 @@ impl Program {
                 Instruction::Add(n, v, op) => {
                     let a = Self::resolve_param(&mem, &n);
                     let b = Self::resolve_param(&mem, &v);
-                    mem[op] = (a + b).to_string();
-                },
+                    mem[op] = a + b;
+                }
 
                 Instruction::Multiply(n, v, op) => {
                     let a = Self::resolve_param(&mem, &n);
                     let b = Self::resolve_param(&mem, &v);
-                    mem[op] = (a * b).to_string();
-                },
+                    mem[op] = a * b;
+                }
 
                 Instruction::Input(op) => {
-                    mem[op] = self.input.unwrap_or(0).to_string();
-                },
+                    if let Some(v) = self.get_input() {
+                        mem[op] = v;
+                    } else {
+                        return Err(Error::new(ErrorKind::InvalidInput, "no input available"));
+                    }
+                }
 
                 Instruction::Output(p) => {
                     let val = Self::resolve_param(&mem, &p);
-                    output.push(val.to_string());
-                    //println!("{}", val);
-                },
+                    output.push(val);
+                }
 
                 Instruction::JumpIfTrue(n, v) => {
                     let a = Self::resolve_param(&mem, &n);
@@ -98,7 +142,7 @@ impl Program {
                         ip = b as usize;
                         continue;
                     }
-                },
+                }
 
                 Instruction::JumpIfFalse(n, v) => {
                     let a = Self::resolve_param(&mem, &n);
@@ -107,33 +151,35 @@ impl Program {
                         ip = b as usize;
                         continue;
                     }
-                },
+                }
 
                 Instruction::LessThan(n, v, op) => {
                     let a = Self::resolve_param(&mem, &n);
                     let b = Self::resolve_param(&mem, &v);
                     if a < b {
-                        mem[op] = "1".to_string();
+                        mem[op] = 1;
                     } else {
-                        mem[op] = "0".to_string();
+                        mem[op] = 0;
                     }
-                },
+                }
 
                 Instruction::Equals(n, v, op) => {
                     let a = Self::resolve_param(&mem, &n);
                     let b = Self::resolve_param(&mem, &v);
                     if a == b {
-                        mem[op] = "1".to_string();
+                        mem[op] = 1;
                     } else {
-                        mem[op] = "0".to_string();
+                        mem[op] = 0;
                     }
-                },
+                }
 
-                Instruction::HCF => { break; },
+                Instruction::HCF => {
+                    break;
+                }
 
                 Instruction::BadInput(v) => {
                     return Err(Error::new(ErrorKind::InvalidInput, format!("{:?}", v)));
-                },
+                }
             };
 
             ip += nparams + 1;
@@ -152,14 +198,13 @@ impl Program {
     // # Panics
     //
     // This method will panic if `param` is a `Parameter::UnexpectedMode`.
-    fn resolve_param(mem: &Vec<String>, param: &Parameter) -> i64 {
+    fn resolve_param(mem: &Vec<i64>, param: &Parameter) -> i64 {
         match param {
-            Parameter::Positional(i) => {
-                let v: i64 = mem[*i].parse().unwrap();
-                v
-            },
+            Parameter::Positional(i) => mem[*i],
             Parameter::Immediate(n) => *n,
-            Parameter::UnexpectedMode(m) => { panic!("unexpected mode: {}", m); },
+            Parameter::UnexpectedMode(m) => {
+                panic!("unexpected mode: {}", m);
+            }
         }
     }
 
@@ -168,9 +213,12 @@ impl Program {
     /// an error is returned.
     pub fn set_mem(&mut self, position: usize, value: i64) -> Result<()> {
         if position >= self.mem.len() {
-            return Err(Error::new(ErrorKind::InvalidInput, "position is out of range"));
+            return Err(Error::new(
+                ErrorKind::InvalidInput,
+                "position is out of range",
+            ));
         }
-        self.mem[position] = value.to_string();
+        self.mem[position] = value;
         Ok(())
     }
 }
@@ -191,7 +239,7 @@ enum Instruction {
 
     /// BadInput is returned by `Instruction::parse` when there are too many,
     /// or too few parameters provided.
-    BadInput(Vec<String>),
+    BadInput(Vec<i64>),
 }
 
 impl Instruction {
@@ -200,7 +248,7 @@ impl Instruction {
     /// calling `Instruction::num_params` on the instruction.
     ///
     /// For example, the short program `1002,4,3,4,33`, is parsed like so:
-    /// 
+    ///
     /// ```ignore
     /// ABCDE
     ///  1002,4,3,4
@@ -216,90 +264,74 @@ impl Instruction {
     /// In plain English, this multiply instruction would multiply the value
     /// at program position 4, with the immediate value 3, and
     /// store the result in position 4.
-    fn parse(v: Vec<String>) -> Self {
-        let np = Self::num_params(&v[0]);
+    fn parse(v: Vec<i64>) -> Self {
+        let np = Self::num_params(v[0]);
         if np + 1 != v.len() {
             return Self::BadInput(v);
         }
 
         // Collect our parameters.
-        //
-        // Reverse the instruction so we can iterate through each character
-        // naturally, and assume any missing parameter mode is a 0 (zero).
-        //
-        // We are also going to slice up the instruction to collect all of the
-        // parameter modes, after the opcode.
-        // This code looks a little wonky, but what it is doing, is making sure
-        // we have 2 (two) characters for the opcode.
-        // If the second character for the opcode is '-', this means the second
-        // character is an implicit 0 (zero), and not an explicit 0.
         let mut params: Vec<Parameter> = vec![];
-        let instruction: String = v[0].chars().rev().collect();
-        let modes: &str = match instruction.chars().nth(1).unwrap_or('-') {
-            '-' => "",
-            _ => &instruction[2..],
-        };
         for i in 0..np {
-            let c = modes.chars().nth(i).unwrap_or('0');
-            let param = match c {
-                '0' => {
-                    // Positional/reference.
-                    let val: usize = v[i+1].parse().unwrap();
-                    Parameter::Positional(val)
-                },
-
-                '1' => {
-                    // Immediate.
-                    let val: i64 = v[i+1].parse().unwrap();
-                    Parameter::Immediate(val)
-                },
-
-                _ => { Parameter::UnexpectedMode(c) },
+            // Figure out the "mode" for the current parameter we are consuming.
+            // 0 => (default) Positional mode.
+            // 1 => Immediate mode.
+            let mode = (v[0] / 10_i64.pow(i as u32 + 2)) % 10;
+            let param = match mode {
+                0 => Parameter::Positional(v[i + 1] as usize),
+                1 => Parameter::Immediate(v[i + 1]),
+                _ => Parameter::UnexpectedMode(mode),
             };
-
             params.push(param);
         }
 
-        // Get the output parameter (which is always the last instruction
-        // argument).
-        // Note that this value isn't always used; it depends on the opcode.
-        let op: usize = v[v.len()-1].parse().unwrap();
+        // Parse the final argument as an output pointer.
+        // Enough instructions use this to know which memory location to store
+        // their output, that it makes sense to parse it all the time.
+        let optr: usize = v[v.len() - 1] as usize;
 
         // Finally, return an Instruction.
-        match instruction.chars().nth(0).unwrap() {
-            '1' => Self::Add(params[0], params[1], op),
-            '2' => Self::Multiply(params[0], params[1], op),
-            '3' => Self::Input(op),
-            '4' => Self::Output(params[0]),
-            '5' => Self::JumpIfTrue(params[0], params[1]),
-            '6' => Self::JumpIfFalse(params[0], params[1]),
-            '7' => Self::LessThan(params[0], params[1], op),
-            '8' => Self::Equals(params[0], params[1], op),
-            '9' => Self::HCF,
+        match Instruction::ones(v[0]) {
+            1 => Self::Add(params[0], params[1], optr),
+            2 => Self::Multiply(params[0], params[1], optr),
+            3 => Self::Input(optr),
+            4 => Self::Output(params[0]),
+            5 => Self::JumpIfTrue(params[0], params[1]),
+            6 => Self::JumpIfFalse(params[0], params[1]),
+            7 => Self::LessThan(params[0], params[1], optr),
+            8 => Self::Equals(params[0], params[1], optr),
+            9 => Self::HCF,
             _ => Self::BadInput(v),
         }
     }
 
-    // Returns the number of parameters that should be consumed by the
-    fn num_params(s: &str) -> usize {
-        let r: String = s.chars().rev().collect();
-        match r.chars().nth(0).unwrap() {
-            '1' => 3, // addition
-            '2' => 3, // multiplication
-            '3' => 1, // input
-            '4' => 1, // output
-            '5' => 2, // jump-if-true
-            '6' => 2, // jump-if-false
-            '7' => 3, // less-than
-            '8' => 3, // equals
-            '9' => {
-                if r.chars().nth(1).unwrap_or('x') == '9' {
-                    return 0;
-                }
-                panic!("Expected opcode 99 from {}", s);
-            }, // end program
-            _ => { panic!("Bad instruction {}", s); },
+    // Returns the number of parameters that should be consumed by the opcode.
+    fn num_params(opcode: i64) -> usize {
+        if opcode == 99 {
+            return 0;
         }
+
+        match Instruction::ones(opcode) {
+            1 => 3, // addition
+            2 => 3, // multiplication
+            3 => 1, // input
+            4 => 1, // output
+            5 => 2, // jump-if-true
+            6 => 2, // jump-if-false
+            7 => 3, // less-than
+            8 => 3, // equals
+            _ => {
+                panic!("Bad opcode {}", opcode);
+            }
+        }
+    }
+
+    fn ones(n: i64) -> i64 {
+        n % 10
+    }
+
+    fn tens(n: i64) -> i64 {
+        (n / 10) % 10
     }
 }
 
@@ -313,37 +345,20 @@ enum Parameter {
     Immediate(i64),
 
     // Indicates the parameter has an unexpected mode.
-    UnexpectedMode(char),
+    UnexpectedMode(i64),
 }
 
 #[test]
 fn test_num_params() {
-    assert_eq!(Instruction::num_params("1"), 3);
-    assert_eq!(Instruction::num_params("2"), 3);
-    assert_eq!(Instruction::num_params("3"), 1);
-    assert_eq!(Instruction::num_params("4"), 1);
-    assert_eq!(Instruction::num_params("5"), 2);
-    assert_eq!(Instruction::num_params("6"), 2);
-    assert_eq!(Instruction::num_params("7"), 3);
-    assert_eq!(Instruction::num_params("8"), 3);
-    assert_eq!(Instruction::num_params("99"), 0);
-}
-
-#[test]
-fn test_program() {
-    use std::collections::HashMap;
-
-    let mut tests: HashMap<&str, &str> = HashMap::new();
-    tests.insert("3,0,4,0,99", "1");
-    tests.insert("1002,4,3,4,33", "1002");
-    tests.insert("1101,100,-1,4,0", "1101");
-    
-    for (inp, want) in &tests {
-        let input = dbg!(String::from(*inp));
-        let mut p = Program::from(input);
-        let (mem, _output) = p.input(1).execute().unwrap();
-        assert_eq!(&mem[0], want);
-    }
+    assert_eq!(Instruction::num_params(1), 3);
+    assert_eq!(Instruction::num_params(2), 3);
+    assert_eq!(Instruction::num_params(3), 1);
+    assert_eq!(Instruction::num_params(4), 1);
+    assert_eq!(Instruction::num_params(5), 2);
+    assert_eq!(Instruction::num_params(6), 2);
+    assert_eq!(Instruction::num_params(7), 3);
+    assert_eq!(Instruction::num_params(8), 3);
+    assert_eq!(Instruction::num_params(99), 0);
 }
 
 #[test]
@@ -354,11 +369,11 @@ fn test_eq_8_positional() {
 
     let (_, out) = program.input(8).execute().unwrap();
     assert_eq!(out.len(), 1);
-    assert_eq!(&out[0], "1");
+    assert_eq!(out[0], 1);
 
     let (_, out) = program.input(88).execute().unwrap();
     assert_eq!(out.len(), 1);
-    assert_eq!(&out[0], "0");
+    assert_eq!(out[0], 0);
 }
 
 #[test]
@@ -369,11 +384,11 @@ fn test_lt_8_positional() {
 
     let (_, out) = program.input(7).execute().unwrap();
     assert_eq!(out.len(), 1);
-    assert_eq!(&out[0], "1");
+    assert_eq!(out[0], 1);
 
     let (_, out) = program.input(88).execute().unwrap();
     assert_eq!(out.len(), 1);
-    assert_eq!(&out[0], "0");
+    assert_eq!(out[0], 0);
 }
 
 #[test]
@@ -384,11 +399,11 @@ fn test_eq_8_immediate() {
 
     let (_, out) = program.input(8).execute().unwrap();
     assert_eq!(out.len(), 1);
-    assert_eq!(&out[0], "1");
+    assert_eq!(out[0], 1);
 
     let (_, out) = program.input(88).execute().unwrap();
     assert_eq!(out.len(), 1);
-    assert_eq!(&out[0], "0");
+    assert_eq!(out[0], 0);
 }
 
 #[test]
@@ -399,11 +414,11 @@ fn test_lt_8_immediate() {
 
     let (_, out) = program.input(7).execute().unwrap();
     assert_eq!(out.len(), 1);
-    assert_eq!(&out[0], "1");
+    assert_eq!(out[0], 1);
 
     let (_, out) = program.input(88).execute().unwrap();
     assert_eq!(out.len(), 1);
-    assert_eq!(&out[0], "0");
+    assert_eq!(out[0], 0);
 }
 
 #[test]
@@ -412,13 +427,13 @@ fn test_jump_positional() {
     let code = String::from("3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9");
     let mut program = Program::from(code);
 
-    let (_, out) = program.execute().unwrap();
+    let (_, out) = program.input(0).execute().unwrap();
     assert_eq!(out.len(), 1);
-    assert_eq!(&out[0], "0");
+    assert_eq!(out[0], 0);
 
     let (_, out) = program.input(88).execute().unwrap();
     assert_eq!(out.len(), 1);
-    assert_eq!(&out[0], "1");
+    assert_eq!(out[0], 1);
 }
 
 #[test]
@@ -427,13 +442,13 @@ fn test_jump_immediate() {
     let code = String::from("3,3,1105,-1,9,1101,0,0,12,4,12,99,1");
     let mut program = Program::from(code);
 
-    let (_, out) = program.execute().unwrap();
+    let (_, out) = program.input(0).execute().unwrap();
     assert_eq!(out.len(), 1);
-    assert_eq!(&out[0], "0");
+    assert_eq!(out[0], 0);
 
     let (_, out) = program.input(88).execute().unwrap();
     assert_eq!(out.len(), 1);
-    assert_eq!(&out[0], "1");
+    assert_eq!(out[0], 1);
 }
 
 #[test]
@@ -441,7 +456,7 @@ fn test_large_example() {
     // This program should return
     //
     // * 999 if the input is less-than 8;
-    // * 1000 if the input is equal to 8; 
+    // * 1000 if the input is equal to 8;
     // * 1001 if the input is greater-than 8.
     //
     let code = String::from("3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99");
@@ -449,14 +464,13 @@ fn test_large_example() {
 
     let (_mem, output) = program.input(7).execute().unwrap();
     assert_eq!(output.len(), 1);
-    assert_eq!(&output[0], "999");
+    assert_eq!(output[0], 999);
 
     let (_mem, output) = program.input(8).execute().unwrap();
     assert_eq!(output.len(), 1);
-    assert_eq!(&output[0], "1000");
+    assert_eq!(output[0], 1000);
 
     let (_mem, output) = program.input(9).execute().unwrap();
     assert_eq!(output.len(), 1);
-    assert_eq!(&output[0], "1001");
+    assert_eq!(output[0], 1001);
 }
-
